e0930ed464b845d1e8fe710f6fe0c85f63d96bb7
Craig Walker <wryguy@thoughtgasm.net>
2017-09-22 20:47:48 +0000

    Error handler

---
+++ js/errorHandling.js
@@ -0,0 +1,11 @@
+Absorb.GroceryCo.Checkout.ErrorHandling = class {
+    static log(reason){
+        console.error(reason);
+
+        const $notifications = document.getElementById("notifications");
+        if ($notifications) {
+            const notification = new Absorb.GroceryCo.Checkout.Notification(String(reason), 'error');
+            Absorb.GroceryCo.Checkout.Notifications.add(notification);
+        }
+    }
+}
