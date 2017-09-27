d0197199b4e547e49d97b3b9a7cfa3f75051c614
Craig Walker <wryguy@thoughtgasm.net>
2017-09-27 01:33:39 +0000

    Basket model

---
+++ spec/js/basketSpec.js
@@ -0,0 +1,237 @@
+describe("Absorb.GroceryCo.Checkout.Basket", function() {
+    beforeEach(function() {
+        this.instance = new Absorb.GroceryCo.Checkout.Basket();
+    });
+
+    describe("createPromotion()", function() {
+        beforeEach(function() {
+            this.id = "test-promotion-id";
+            this.price = 7;
+            this.quantity = 11;
+            this.discount = 0.13;
+            this.create = () => {
+                this.promotionData = {
+                    discount: this.discount,
+                    id: this.id,
+                    price: this.price,
+                    quantity: this.quantity,
+                    type: this.type,
+                };
+                this.result = this.instance.createPromotion(this.promotionData);
+                // In this case, the subject is always the result, so we can 
+                // just assign it here.
+                this.subject = this.result;
+            };
+        });
+
+        function itSetsTheId() {
+            it("has the ID property set", function() {
+                expect(this.subject.id).toEqual(this.id);
+            });
+        }
+
+        function itSetsThePrice() {
+            it("has the price property set", function() {
+                expect(this.subject.price).toEqual(this.price);
+            });
+        }
+
+        function itSetsTheQuantity() {
+            it("has the quantity property set", function() {
+                expect(this.subject.quantity).toEqual(this.quantity);
+            });
+        }
+
+        function itSetsTheDiscount() {
+            it("has the discount property set", function() {
+                expect(this.subject.discount).toEqual(this.discount);
+            });
+        }
+
+        describe("when the type is sale", function() {
+            beforeEach(function() {
+                this.type = "sale";
+                this.create();
+            });
+
+            describe("the created promotion", function() {
+                it("is a Sale", function() {
+                    expect(this.subject instanceof Absorb.GroceryCo.Checkout.Promotions.Sale).toEqual(true);
+                });
+                itSetsTheId();
+                itSetsThePrice();
+            });
+        });
+        describe("when the type is group", function() {
+            beforeEach(function() {
+                this.type = "group";
+                this.create();
+            });
+
+            describe("the created promotion", function() {
+                it("is a Group", function() {
+                    expect(this.subject instanceof Absorb.GroceryCo.Checkout.Promotions.Group).toEqual(true);
+                });
+                itSetsTheId();
+                itSetsThePrice();
+                itSetsTheQuantity();
+            });
+        });
+        describe("when the type is additionalProduct", function() {
+            beforeEach(function() {
+                this.type = "additionalProduct";
+                this.create();
+            });
+
+            describe("the created promotion", function() {
+                it("is an AdditionalProduct", function() {
+                    expect(this.subject instanceof Absorb.GroceryCo.Checkout.Promotions.AdditionalProduct).toEqual(true);
+                });
+                itSetsTheId();
+                itSetsTheDiscount();
+                itSetsTheQuantity();
+            });
+        });
+        describe("when the type unknown", function() {
+            beforeEach(function() {
+                this.type = "something else";
+                this.create();
+            });
+
+            it("returns null", function() {
+                expect(this.result).toBeNull();
+            });
+        });
+    });
+
+    describe("load()", function() {
+        beforeEach(function() {
+            this.promotionsData = {
+                promotions: []
+            };
+            this.basketData = {
+                basket: []
+            };
+            this.load = () => {
+                this.result = this.instance.load(this.inventoryData, this.promotionsData, this.basketData);
+            };
+        });
+
+        describe("when passed inventory product data", function() {
+            beforeEach(function() {
+                this.productId = "test-product";
+                this.productPrice = 3;
+                this.inventoryData = {
+                    products: [
+                        {
+                            id: this.productId,
+                            price: this.productPrice
+                        }
+                    ]
+                };
+            });
+
+            describe("and matching basket item data", function() {
+                beforeEach(function() {
+                    this.basketQuantity = 5;
+                    this.basketData = {
+                        basket: [
+                            {
+                                productId: this.productId,
+                                quantity: this.basketQuantity
+                            }
+                        ]
+                    };
+                });
+
+                describe("the first item", function() {
+                    beforeEach(function() {
+                        this.load();
+                        this.subject = this.instance.items[0];
+                    });
+
+                    it("is a BasketItem", function() {
+                        // In general, duck typing is a good thing,
+                        // but I'll frequently do type check tests
+                        // just to ensure that we're getting an object 
+                        // that's also under tests. If it matters,
+                        // I'll write the appropriate tests to check for
+                        // duck typed methods; this way is just quicker.
+                        // 
+                        // Note that I usually like to install/write a
+                        // toBeInstanceOf() check into Jasmine so that,
+                        // if it fails, I can see what the object's actual
+                        // class/prototype is. This just gives me a "false".
+                        expect(this.subject instanceof Absorb.GroceryCo.Checkout.BasketItem).toEqual(true);
+                    });
+
+                    it("has the product id", function() {
+                        expect(this.subject.id).toEqual(this.productId);
+                    });
+
+                    it("has the product price", function() {
+                        expect(this.subject.price).toEqual(this.productPrice);
+                    });
+
+                    it("has no promotions", function() {
+                        expect(this.subject.promotions).toEqual([]);
+                    });
+
+                    it("has the basket quantity", function() {
+                        expect(this.subject.quantity).toEqual(this.basketQuantity);
+                    });
+                });
+
+                describe("and matching promotions", function() {
+                    beforeEach(function() {
+                        this.promotionData = {
+                            productId: this.productId
+                        };
+                        this.promotionsData = {
+                            promotions: [
+                                this.promotionData
+                            ]
+                        };
+                        this.promotion = {
+                            id: "A promotion object"
+                        };
+                        spyOn(this.instance, "createPromotion").and.returnValue(this.promotion);
+                        this.load();
+                    });
+
+                    it("passes the promotion data to createPromotion", function() {
+                        expect(this.instance.createPromotion).toHaveBeenCalledWith(this.promotionData);
+                    });
+
+                    describe("the first item", function() {
+                        beforeEach(function() {
+                            this.subject = this.instance.items[0];
+                        });
+
+                        it("has the created promotion", function() {
+                            expect(this.subject.promotions).toEqual([this.promotion]);
+                        });
+                    });
+                });
+            });
+
+            describe("and no matching basket item data", function() {
+                beforeEach(function() {
+                    this.basketData = {
+                        basket: []
+                    };
+                });
+
+                describe("the items", function() {
+                    beforeEach(function() {
+                        this.subject = this.instance.items;
+                    });
+
+                    it("are empty", function() {
+                        expect(this.subject).toEqual([]);
+                    });
+                });
+            });
+        });
+    });
+});
--- src/js/basket.js
+++ src/js/basket.js
@@ -3,7 +3,104 @@ Absorb.GroceryCo.Checkout.Basket = class {
         this.items = [];
     }
 
-    load(inventoryData, promotionData, basketData){
+    createPromotion(promotionData) {
+        switch (promotionData.type) {
+            case "sale":
+                return new Absorb.GroceryCo.Checkout.Promotions.Sale(
+                    promotionData.id,
+                    promotionData.price
+                );
+            case "group":
+                return new Absorb.GroceryCo.Checkout.Promotions.Group(
+                    promotionData.id,
+                    promotionData.quantity,
+                    promotionData.price
+                );
+            case "additionalProduct":
+                return new Absorb.GroceryCo.Checkout.Promotions.AdditionalProduct(
+                    promotionData.id,
+                    promotionData.quantity,
+                    promotionData.discount
+                );
+            default:
+                // Another potential for discrepancy between server and client. What if
+                // the server returns a promotion type that's not defined on the client?
+                // Does that mean the server is broken, or is the client out of date?
+                // That's a bigger question than we're prepared to deal with for this app;
+                // for now I'll just ignore the promotion entirely.
+                return null;
+        }
+    }
 
+    load(inventoryData, promotionsData, basketData) {
+        const productDataById = inventoryData.products.reduce((map, productData) => {
+            map[productData.id] = productData;
+            return map;
+        }, {});
+
+        promotionsData.promotions.reduce((map, promotionData) => {
+            const productData = map[promotionData.productId];
+            if (productData) {
+                // How much should the client care about problems caused by the server?
+                // For example, if promotions.json includes a promotion for a product
+                // that doesn't exist in inventory.json, then there's some sort of 
+                // odd data inconsistency going on somewhere.
+                //
+                // How should the client handle this? Well, it definitely shouldn't 
+                // merely break (ie: throw an unhandled exception such as 
+                // "Cannot read property 'promotions' of productData"). So we'd better
+                // be guarding against the possibility of a non-existent product
+                // (either through a conditional like I have done here, or possibly through
+                // default values).
+                //
+                // In this case, it's pretty reasonable to ignore the problematic condition.
+                // Without a product, a promotion is pretty useless, so we might as well 
+                // just skip over it. 
+                // 
+                // But should we inform the user? For instance, should we add an "else"
+                // clause to the above if and then log an error/warning message?
+                // In cases like these, I say no, because the user can't do anything 
+                // to resolve the condition themselves; this is entirely a problem in the 
+                // data (and ultimately a problem with the server's data validation), so 
+                // pestering the user with error message will just be annoying.
+                // 
+                // We might make the argument that we should send an error message
+                // (invisble to the user) from the client back to the server, so that
+                // developers / admins will see the error and take steps to fix it. 
+                // This assumes that there's a mechanism for reporting client-side 
+                // error messages on the server -- and that people are actually paying
+                // attention to it.
+                //
+                // Since we have no server for this app, there's not much value in 
+                // creating a (dummy) reporting mechanism. (And we control the data anyway
+                // so there's little risk of it being an issue in the first place.) Thus, I'm 
+                // choosing to silently ignore data issues like this on the client. 
+                // 
+                // In general though, I prefer to avoid silent error conditions, so I'd consider
+                // raising a fuss here.
+                if (!productData.promotions) {
+                    productData.promotions = [];
+                }
+                const promotion = this.createPromotion(promotionData);
+                if (promotion) {
+                    productData.promotions.push(promotion);
+                }
+            }
+            return map;
+        }, productDataById);
+
+        this.items = basketData.basket.reduce((array, basketItemData) => {
+            const product = productDataById[basketItemData.productId];
+            if (product) {
+                const basketItem = new Absorb.GroceryCo.Checkout.BasketItem(
+                    product.id,
+                    product.price,
+                    product.promotions,
+                    basketItemData.quantity
+                );
+                array.push(basketItem);
+            }
+            return array;
+        }, []);
     }
 };
