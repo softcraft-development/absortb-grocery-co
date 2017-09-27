13fdfd55d355deee70a2ed3c0b51d0e0517c7018
Craig Walker <wryguy@thoughtgasm.net>
2017-09-27 22:33:14 +0000

    Custom matchers for element tests

---
+++ spec/customMatchers.js
@@ -0,0 +1,40 @@
+Absorb.GroceryCo.Checkout.Test = {
+    customMatchers: {
+        toHaveTagName(util, customEqualityTesters) {
+            return {
+                compare($element, tagName) {
+                    const expected = tagName.toLowerCase();
+                    const actual = $element.tagName.toLowerCase();
+                    const result = {
+                        pass: util.equals(actual, expected, customEqualityTesters)
+                    };
+                    if (result.pass) {
+                        result.message = `Expected element to not be <${actual}>`;
+                    } else {
+                        result.message = `Expected element <${expected}> to be <${actual}>`;
+                    }
+                    return result;
+                }
+            };
+        },
+        toHaveTagCssClass() {
+            return {
+                compare($element, className) {
+                    const result = {
+                        pass: $element.classList.contains(className)
+                    };
+                    if (result.pass) {
+                        result.message = `Expected element to not have class ${className}`;
+                    } else {
+                        result.message = `Expected element to have class ${className}; actual classes are: '${$element.className}'`;
+                    }
+                    return result;
+                }
+            };
+        },
+    }
+};
+
+beforeEach(function() {
+    jasmine.addMatchers(Absorb.GroceryCo.Checkout.Test.customMatchers);
+});
